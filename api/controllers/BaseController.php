6be2dfe2c6d0355361ac90fba45c798a8861877e
Peçanha, Bruno <112444866+PecanhaBruno@users.noreply.github.com>
2026-07-22 00:58:20 -0300

    Ajusta nome de arquivos e classes

---
+++ api/controllers/BaseController.php
@@ -0,0 +1,24 @@
+<?php
+
+abstract class BaseController
+{
+    protected function ok($data): void
+    {
+        Response::json($data);
+    }
+
+    protected function badRequest(string $message): void
+    {
+        Response::badRequest($message);
+    }
+
+    protected function unauthorized(string $message = 'Não autorizado.'): void
+    {
+        Response::unauthorized($message);
+    }
+
+    protected function notFound(): void
+    {
+        Response::notFound();
+    }
+}
