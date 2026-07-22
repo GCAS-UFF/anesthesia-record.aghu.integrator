800c6beb9582c368ab7e9333c8e5a8fca8f062b1
Peçanha, Bruno <112444866+PecanhaBruno@users.noreply.github.com>
2026-07-22 00:48:06 -0300

    Add files via upload

---
+++ api/core/Router.php
@@ -0,0 +1,70 @@
+<?php
+
+class Router
+{
+    private array $routes = [];
+
+    public function get(string $route, array $action): void
+    {
+        $this->addRoute('GET', $route, $action);
+    }
+
+    public function post(string $route, array $action): void
+    {
+        $this->addRoute('POST', $route, $action);
+    }
+
+    public function put(string $route, array $action): void
+    {
+        $this->addRoute('PUT', $route, $action);
+    }
+
+    public function delete(string $route, array $action): void
+    {
+        $this->addRoute('DELETE', $route, $action);
+    }
+
+    private function addRoute(string $method, string $route, array $action): void
+    {
+        $this->routes[] = [
+            'method' => $method,
+            'route' => $route,
+            'action' => $action
+        ];
+    }
+
+    public function dispatch(): void
+    {
+        $method = $_SERVER['REQUEST_METHOD'];
+
+        $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+        // Remove a pasta public caso exista na URL
+        $uri = str_replace('/public', '', $uri);
+
+        foreach ($this->routes as $route) {
+
+            if ($route['method'] !== $method) {
+                continue;
+            }
+
+            $pattern = preg_replace('/\{[^\/]+\}/', '([^/]+)', $route['route']);
+            $pattern = '#^' . $pattern . '$#';
+
+            if (preg_match($pattern, $uri, $matches)) {
+
+                array_shift($matches);
+
+                [$controller, $method] = $route['action'];
+
+                $instance = new $controller();
+
+                call_user_func_array([$instance, $method], $matches);
+
+                return;
+            }
+        }
+
+        Response::notFound();
+    }
+}
