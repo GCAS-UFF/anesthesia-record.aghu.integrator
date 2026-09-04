e26c6857929776a321f12663d5067bb5e593cf46
Bruno Peçanha <brunoitdesenv@gmail.com>
2026-09-04 17:55:48 -0300

    Ajusta recuperação de db por variaveis de ambiente

--- api/config/config.php
+++ api/config/config.php
@@ -1,34 +1,25 @@
 <?php
 
 return [
-    // 'db' => [
-        // 'driver'   => 'pgsql',
-        // 'host'     => getenv('DB_HOST'),
-		// 'port'     => getenv('DB_PORT'),
-		// 'database' => getenv('DB_DATABASE'),
-		// 'schema'   => getenv('DB_SCHEMA'),
-		// 'username' => getenv('DB_USERNAME'),
-		// 'password' => getenv('DB_PASSWORD'),
-    // ],
-	// 'db' => [
-		// 'driver'   => 'pgsql',
-		// 'host'     => 'localhost',
-		// 'port'     => '5432',
-		// 'database' => 'postgres',
-		// 'schema'   => 'aghu_stg',
-		// 'username' => 'postgres',
-		// 'password' => '13676616766'
-	// ],
-	'db' => [
-		'driver'   => 'pgsql',
-		'host'     => 'aws-0-us-east-2.pooler.supabase.com',
-		'port'     => '5432',
-		'database' => 'postgres',
-		'schema'   => 'aghu_stg',
-		'username' => 'postgres.yidudaduvasngangrydi',
-		'password' => '13676616766m&b'
-	],
-
+     'db' => [
+         'driver'   => 'pgsql',
+         'host'     => getenv('DB_HOST'),
+		 'port'     => getenv('DB_PORT'),
+		 'database' => getenv('DB_DATABASE'),
+		 'schema'   => getenv('DB_SCHEMA'),
+		 'username' => getenv('DB_USERNAME'),
+		 'password' => getenv('DB_PASSWORD'),
+    	],
+	//  'db' => [
+	// 	 'driver'   => 'pgsql',
+	// 	 'host'     => 'localhost',
+	// 	 'port'     => '5432',
+	// 	 'database' => 'postgres',
+	// 	 'schema'   => 'aghu_stg',
+	// 	 'username' => 'postgres',
+	// 	 'password' => '13676616766'
+	//  ],
+	
     'api' => [
         'basePath' => '/public',
         'timezone' => 'America/Sao_Paulo',
